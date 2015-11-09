27f0730458dd58ddb0a094ce200f673a686949de
Brian Walters <brianw@pixotech.com>
2015-11-09 17:28:24 -0600

    Hello world

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,38 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     var taskConfig = {
-        copy: {
+        connect: {
+            options: {
+                hostname: 'localhost',
+                base: 'build/',
+                livereload: 35729
+            },
+
             build: {
+                options: {
+                    open: 'http://localhost:9999',
+                    port: 9999
+                }
+            }
+        },
+        copy: {
+            html: {
                 files: [{
                     cwd: 'src/',
-                    src: 'index.html',
+                    src: ['**/*.html'],
                     dest: 'build/',
                     expand: true
                 }]
+            }
+        },
+        watch: {
+            html: {
+                files: ['src/**/*.html'],
+                tasks: ['copy:html']
+            },
+            js: {
+                files: ['src/**/*.js'],
+                tasks: ['copy:js']
             }
         }
     };
@@ -20,7 +44,17 @@ module.exports = function(grunt) {
         'build'
     ]);
 
-    grunt.registerTask('build', [
+    grunt.registerTask('build', 'Development build of the app', [
         'copy'
+    ]);
+
+    grunt.registerTask('dev', 'Build app and begin development', [
+        'build',
+        'connect',
+        'watch'
+    ]);
+
+    grunt.registerTask('serve', 'Alias for dev task', [
+        'dev'
     ]);
 };
